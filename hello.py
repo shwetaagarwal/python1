da1baec804d3dc5c29e3a1d4ffd95ff0b7f62d29
Shweta Agarwal <shweta.agarwal@gandlake.com>
2017-04-24 11:00:52 +0100

    conditionals, casting, arithmetic

--- hello.py
+++ hello.py
@@ -3,7 +3,32 @@ import random
 #name="Shweta"
 print("enter your name:")
 name = input()
-print("Dear "+name);
+#print("Dear "+name);
+message = "Dear {0}"
+print(message.format(name))
 
-aRandomNum = random.randint(4,10)
-print(aRandomNum)
+minNum=4
+maxNum=10
+aRandomNum = random.randint(minNum,maxNum)
+message = "Guess a number between {0} and {1}"
+print(message.format(minNum,maxNum))
+
+
+rightGuess = False
+tryCount = 0
+while tryCount < 3 and not rightGuess:
+    guessedNum = int(input())
+    tryCount += 1
+    if guessedNum == aRandomNum:
+        rightGuess = True
+        #break
+    elif guessedNum < aRandomNum:
+        print("too less!")
+    elif guessedNum > aRandomNum:
+        print("too high!")
+
+if rightGuess:
+    print("you guessed right in " + str(tryCount) + "trie(s)")
+else:
+    print("sorry, you couldn't guess the number "+ str(aRandomNum) + " within " + str(tryCount) + " tries.")
+
